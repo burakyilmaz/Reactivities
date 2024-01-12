9f124faf770a1a8035d5867d3a643c390fd63933
Burak Yılmaz <yilmazbrk@outlook.com>
2024-01-13 00:37:22 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -23,6 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 app.MapControllers();
 
@@ -38,7 +46,7 @@ try
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An error occurred during migration");
+    logger.LogError(ex, "An error occurred during migration");
 }
 
 app.Run();
